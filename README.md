51d85aad924d3a623a1eaa0333aa15aeb16589f8
dlanoronald123 <dlanoronald123@gmail.com>
2022-07-26 20:38:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-d4jiwv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-d4jiwv)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
